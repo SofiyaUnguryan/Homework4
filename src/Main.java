b96cc1ee7f5dcbe660937a8b33756aef7f762fd2
Sofiya.Unguryan <sofya.unguryan@yandex.ru>
2023-07-18 18:44:44 +0300

    Almost initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 40;
+        if (age >= 18) {
+            System.out.println("Вы совершеннолетний");
+        } else {
+            System.out.println("Вы пока не совершеннолетний, нужно немного подождать");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int temperature = 14;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        int speed = 67;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+    }
+}
